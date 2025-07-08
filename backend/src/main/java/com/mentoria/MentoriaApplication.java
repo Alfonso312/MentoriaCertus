bf49dc342f27b418648e211cfaee1c6ec8a63bd2
Alfonso <c-walk5@gmail.com>
2025-07-08 10:18:07 -0500

    Proyecto completo Mentoria Certus actualizado

---
+++ backend/src/main/java/com/mentoria/MentoriaApplication.java
@@ -0,0 +1,12 @@
+package com.mentoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Proyecto: Mentoria Certus
+@SpringBootApplication
+public class MentoriaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MentoriaApplication.class, args);
+    }
+} 
